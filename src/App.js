9c27f39a15036816fd0e244fa36034937f119c88
Ishara <122250728+ishara87@users.noreply.github.com>
2025-04-12 16:39:05 +0530

    Final OCR Fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient';
 import Webcam from 'react-webcam';
 import Tesseract from 'tesseract.js';
