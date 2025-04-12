107dd7ef0f85639cb2d98804458bad277550632e
Ishara <122250728+ishara87@users.noreply.github.com>
2025-04-12 15:18:32 +0530

    Test Build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
-    return (
-      <div style={{ padding: '2rem', fontSize: '24px' }}>
-        ✅ CleanDrive is running!
-      </div>
-    );
-  } 
+  return (
+    <div style={{ padding: '2rem', fontSize: '24px' }}>
+      ✅ CleanDrive is running!
+    </div>
+  );
+}
